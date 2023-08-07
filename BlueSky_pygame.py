1bc75025966f49be346f31822dd3b927b46ecb44
dah4k <84741290+dah4k@users.noreply.github.com>
2023-08-07 01:22:47 -0400

    pygame_argparse: Add cmdargs.parse()
    - Disable first time Keyboard update when SCENFILE is already configured
    - Add PDEBUG() with data pretty printer capability

--- BlueSky_pygame.py
+++ BlueSky_pygame.py
@@ -4,14 +4,39 @@ from __future__ import print_function
 import pygame as pg
 import bluesky as bs
 from bluesky.ui.pygame import splash
+from bluesky import cmdargs
+from pprint import pprint
 
+
+def PDEBUG(msg, obj=None):
+    print("\033[0;34m[DEBUG] " + msg)
+    if obj is not None:
+        pprint(obj)
+    print("\033[0m", end='')
 
 def main():
+    # HACK: Parse BlueSky Pygame arguments using BlueSky QtGL cmdargs
+    args = cmdargs.parse()
+    args['gui'] = 'pygame'
+    args['mode'] = 'sim'
+    PDEBUG("Program arguments", args)
+
     """ Start the mainloop (and possible other threads) """
     splash.show()
-    bs.init(gui='pygame')
+    bs.init(**args)
     # bs.sim.op()
     bs.scr.init()
+    PDEBUG("Screen is initialized", bs.scr)
+
+    if args['scenfile'] is not None:
+        # Disable first time Keyboard.update()
+        #
+        # The first time Keyboard.update() would emit an empty 'IC' command
+        # to reset the simulation with a Screen.show_file_dialog().
+        # Simulation.init() already has scheduled an f'IC {scenfile}' command
+        # so there is no need to Screen.show_file_dialog().
+        bs.scr.keyb.firstx = False
+    PDEBUG("Keyboard.firstx", bs.scr.keyb.firstx)
 
     # Main loop for BlueSky
     while not bs.sim.state == bs.END:
